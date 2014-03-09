f8ced9fd2a4b745cc31d0664de39814daa643db7
ltf1320 <327105504@qq.com>
2014-03-09 13:12:03 +0800

    basic

--- TeachingCommunicationPlatform_web/MasterPages/basic.master.cs
+++ TeachingCommunicationPlatform_web/MasterPages/basic.master.cs
@@ -7,6 +7,24 @@ using System.Web.UI.WebControls;
 
 public partial class MasterPages_basic : System.Web.UI.MasterPage
 {
+    string _userName;
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        if(Session["userName"]==null)
+        {
+            userNameLabel.Visible = false;
+            loginBtn.Text = "登陆";
+        }
+        else
+        {
+            _userName = Session["userName"].ToString();
+            userName.Visible = false;
+            pwd.Visible = false;
+            loginDropList.Visible = false;
+            userNameLabel.Text = _userName;
+            loginBtn.Text = "注销";
+        }
+    }
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -22,4 +40,5 @@ public partial class MasterPages_basic : System.Web.UI.MasterPage
         else
             return;
     }
+
 }
