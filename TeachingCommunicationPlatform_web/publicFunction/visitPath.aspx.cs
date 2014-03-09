633d01c9f2dd57508099fe0ded7b1b135d35b83a
ltf1320 <327105504@qq.com>
2014-03-10 01:00:50 +0800

    sleep

--- TeachingCommunicationPlatform_web/publicFunction/visitPath.aspx.cs
+++ TeachingCommunicationPlatform_web/publicFunction/visitPath.aspx.cs
@@ -7,8 +7,19 @@ using System.Web.UI.WebControls;
 
 public partial class publicFunction_visitCourse : System.Web.UI.Page
 {
+    safeFileManager sFileMana;
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (Session["ha_path"] == null)
+        {
+            Response.Redirect("allCourse.aspx");
+            return;
+        }
+        sFileMana = new safeFileManager();
+        sFileMana.SetRootPath(Session["ha_path"].ToString());
+        if(Session["ha_user"]!=null)
+        {
+            sFileMana.setUser(Session["ha_user"].ToString(), Session["ha_pwd"].ToString());
+        }
     }
 }
