28c1e31b97fb5c7b8135c3f08051eba630ec2d60
YiKou <renzhe04007@gmail.com>
2014-03-12 02:02:50 +0800

    ?

--- TeachingCommunicationPlatform_web/teacher/newThings.aspx.cs
+++ TeachingCommunicationPlatform_web/teacher/newThings.aspx.cs
@@ -13,31 +13,31 @@ public partial class teacher_newThings : System.Web.UI.Page
     List<CMessage> msgList;
     protected void Page_Load(object sender, EventArgs e)
     {
-        sFM = new safeFileManager();
-        if (Session["ha_user"] == null || Session["ha_pwd"] == null)
-            Response.Redirect("publicFunction\allCourse.aspx");
-        user=Session["ha_user"].ToString();
-        sFM.setUser(user, Session["ha_pwd"].ToString());
-        sFM.SetRootPath("users\\" + user);
-        StreamReader rder= sFM.getStreamReader("newThings");
-        msgList=new List<CMessage>();
-        CMessage msg;
-        string couId; int msgId;
-        while(!rder.EndOfStream)
-        {
-            couId = rder.ReadLine();
-            msgId = Convert.ToInt32(rder.ReadLine());
-            msg = new CMessage();
-            msg.getMsgInfo(couId, msgId);
-            if (msg.hasMsg)
-                msgList.Add(msg);
-        }
-        dataBind();
+        //sFM = new safeFileManager();
+        //if (Session["ha_user"] == null || Session["ha_pwd"] == null)
+        //    Response.Redirect("publicFunction\allCourse.aspx");
+        //user=Session["ha_user"].ToString();
+        //sFM.setUser(user, Session["ha_pwd"].ToString());
+        //sFM.SetRootPath("users\\" + user);
+        //StreamReader rder= sFM.getStreamReader("newThings");
+        //msgList=new List<CMessage>();
+        //CMessage msg;
+        //string couId; int msgId;
+        //while(!rder.EndOfStream)
+        //{
+        //    couId = rder.ReadLine();
+        //    msgId = Convert.ToInt32(rder.ReadLine());
+        //    msg = new CMessage();
+        //    msg.getMsgInfo(couId, msgId);
+        //    if (msg.hasMsg)
+        //        msgList.Add(msg);
+        //}
+        //dataBind();
     }
     protected void dataBind()
     {
-        DataList1.DataSource = msgList;
-        DataList1.DataBind();
+        //DataList1.DataSource = msgList;
+        //DataList1.DataBind();
     }
     protected void DataList1_DataBinding(object sender, EventArgs e)
     {
