13ef2da68f73323e0964890d2ea3c96214660aa1
YiKou <renzhe04007@gmail.com>
2014-03-12 01:51:31 +0800

    fix

--- TeachingCommunicationPlatform_web/teacher/myControl.aspx
+++ TeachingCommunicationPlatform_web/teacher/myControl.aspx
@@ -1,22 +1,90 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPages/userMasterPage.master" AutoEventWireup="true" CodeFile="myControl.aspx.cs" Inherits="teacher_myControl" %>
 
-<%-- 列出已关注课程/关注课程（连接到publicFunction\allCourse.aspx)/取消关注--%>
-<%--关注课程:
-找到users下listens文件，添加课程(couId)
-找到courses下listeners文件，添加userId--
-数据库中课程人数+1    --%>
+
+
 
 
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="Server">
-    <table>
-    <asp:GridView ID="GridView1" runat="server">
-    </asp:GridView>
-    <asp:Button ID="focusMoreBtn" runat="server" Height="21px" OnClick="focusMoreBtn_Click" Text="关注更多" Width="77px" />
+    <asp:Panel ID="Panel1" runat="server">
+        <asp:Button ID="focusMoreBtn" runat="server" Height="21px" OnClick="focusMoreBtn_Click" Text="关注更多" Width="77px" />
+        <table style="height: 222px; width: 348px">
+            <tr>
+                <td>
+                    <%--发布消息：
+                    输入消息topic
+                    消息内容
+                    上传文件
+                    @user
+                    找到couse文件夹的message，添加信息
+                    生成新的消息id
+                    在course文件夹下找到listeners，找到所有listeners的user文件夹下的newthings文件，将couId,id添加进去
+                    格式见pd--%>
 
-    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <%-- 列出已关注课程/关注课程（连接到publicFunction\allCourse.aspx)/取消关注--%>
+                    <%--关注课程:
+                    找到users下listens文件，添加课程(couId)
+                    找到courses下listeners文件，添加userId--
+                    数据库中课程人数+1    --%>
+                    <asp:GridView ID="GridView1" runat="server" Height="116px" Width="291px">
+                    </asp:GridView>
 
 
 
+
+                </td>
+
+            </tr>
+
+        </table>
+
+    </asp:Panel>
+
+    <asp:Panel ID="Panel2" runat="server">
+        <table>
+            <tr>
+                <td>
+                    <%--功能：课程管理者
+                    管理课程列表
+                    选中->发布（删除)消息/发布(删除)作业/查看上交作业/添加管理者（!!teacher only)--%>
+
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <%--功能：新建和删除课程(!!teacher only）
+                    新建课程：
+                    输入课程名等
+                    生成新couId(max+1)
+                    在courses文件夹下创建相关文件（夹）--%>
+
+                    <asp:GridView ID="GridView2" runat="server" AutoGenerateColumns="False" DataKeyNames="couId" DataSourceID="SqlDataSource1" OnRowDeleting="GridView2_RowDeleting">
+                        <Columns>
+                            <asp:BoundField DataField="couId" HeaderText="couId" ReadOnly="True" SortExpression="couId" />
+                            <asp:BoundField DataField="couName" HeaderText="couName" SortExpression="couName" />
+                            <asp:BoundField DataField="type" HeaderText="type" SortExpression="type" />
+                            <asp:BoundField DataField="stuNum" HeaderText="stuNum" SortExpression="stuNum" />
+                            <asp:BoundField DataField="term" HeaderText="term" SortExpression="term" />
+                            <asp:BoundField DataField="createUser" HeaderText="createUser" SortExpression="createUser" />
+                            <asp:CommandField ShowDeleteButton="True" />
+                        </Columns>
+                    </asp:GridView>
+                    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:TeachingCommunicationPlatform_DBConnectionString %>" SelectCommand="select * from [Course] where ([couId] in (SELECT couId FROM [manageCou] WHERE ([userId] = @userId)))
+">
+                        <SelectParameters>
+                            <asp:SessionParameter Name="userId" SessionField="ha_user" Type="String" />
+                        </SelectParameters>
+                    </asp:SqlDataSource>
+
+                </td>
+            </tr>
+        </table>
+
+    </asp:Panel>
 
 
 </asp:Content>
@@ -27,27 +95,3 @@
 
 
 
-
-
-
-
-<%--功能：课程管理者
-管理课程列表
-选中->发布（删除)消息/发布(删除)作业/查看上交作业/添加管理者（!!teacher only)--%>
-
-<%--发布消息：
-输入消息topic
-消息内容
-上传文件
-@user
-找到couse文件夹的message，添加信息
-生成新的消息id
-在course文件夹下找到listeners，找到所有listeners的user文件夹下的newthings文件，将couId,id添加进去
-格式见pd--%>
-
-
-<%--功能：新建和删除课程(!!teacher only）
-新建课程：
-输入课程名等
-生成新couId(max+1)
-在courses文件夹下创建相关文件（夹）--%>
--- TeachingCommunicationPlatform_web/teacher/myControl.aspx.cs
+++ TeachingCommunicationPlatform_web/teacher/myControl.aspx.cs
@@ -28,52 +28,97 @@ public partial class teacher_myControl : System.Web.UI.Page
         string focus;
         SqlParameter[] paras = new SqlParameter[lis.Length];
         StringBuilder stb =new StringBuilder();
-        stb.Append("select * from Course where couId = @f0");
+        stb.Append("select couId as 课程编号,couName as 课程名称 , type as 属性 , stuNum as 关注人数 ,term as 学期 , createUser as 教师 from Course where couId = @f0 ");
         paras[0]=new SqlParameter("@f0",lis[0]);
-        DataSet ds = new DataSet();
         for(int i = 1 ; i < lis.Length;i++)
         {
             paras[i] = new SqlParameter("@f" + i, lis[i]);
-            string sqstr="union select * from Cours";
-            stb.Append("")
-
-
-
-
-
-
-
-
-            stb.Append("union");
-            paras[0] = new SqlParameter("@f", focus);
-             SqlDataAdapter da = sqlhp.getAdapter(sqlstr, paras);
-             da.fill(ds);
-
-             GridView1.DataSource = ds.Tables[0].DefaultView;
+            stb.Append(" union select * from Course where couId = @f"+i);
+        }
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = sqlhp.getAdapter(stb.ToString(), paras);
             sqlhp.close();
-        }
-        string SqlStr = "SELECT Cource.*,Teacher.* FROM Cource,Teacher where Cource.teaID=Teacher.teaID order by Teacher.teaID";
-        DataSet ds = new DataSet();
-        SqlConnection conn = new SqlConnection(connStr);
-        try
-        {
-            if (conn.State.ToString() == "Closed")
-                conn.Open();
-            SqlDataAdapter da = new SqlDataAdapter(SqlStr, conn);
-            da.Fill(ds);
+             da.Fill(ds);
+             GridView1.DataSource = ds.Tables[0].DefaultView;
+             GridView1.DataBind();
 
-            GridView1.DataSource = ds.Tables[0].DefaultView;
+     
+    }
+    protected void GridView2_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+
+        string id = GridView2.DataKeys[e.RowIndex].Values[0].ToString(); //取出要删除记录的主键值
+        string delestr1 = "delete from manageCou where couId = @id";
+        string delestr2 = "delete from Course  where couId = @id";
+        SqlParameter[] paras = new SqlParameter[1];
+        paras[0] = new SqlParameter("@id", id);
+        sqlhp.ExecuteSql(delestr1, paras);
+        paras[0] = new SqlParameter("@id", id);
+        sqlhp.ExecuteSql(delestr2, paras);
+        sqlhp.close();
+        sf.SetRootPath("courses");
+        if (!sf.setUser(Session["ha_user"].ToString(), Session["ha_pwd"].ToString()))
+        {
+            Methods.showMessageBox(Response, "对不起您没有权限");
+            return;
+        }
+        /////////////未dug
+        sf.cd(id);
+        string[] canFou = sf.readFile("listeners");
+        for (int i = 0; i < canFou.Length; i++)
+        {
+            sf.SetRootPath("users");
+            sf.cd(canFou[i]);
+            sf.deleteStrFromFile("listens", id);
+
+        }
+        //////////////
+        if (sf.deleteFolder(id))
+        {
             GridView1.DataBind();
+            Methods.showMessageBox(Response, "删除成功");
         }
-        catch (Exception ex)
+        else
         {
-            Response.Write("数据库错误，错误原因：" + ex.Message);
-            Response.End();
+            Methods.showMessageBox(Response, "删除失败");
         }
-        finally
+    }
+    protected void newOneBtn_Click(object sender, EventArgs e)
+    {
+        Panel3.Visible = true;
+    }
+    protected void subBtn_Click(object sender, EventArgs e)
+    {
+        if (!sf.setUser(Session["ha_user"].ToString(), Session["ha_pwd"].ToString()))
         {
-            if (conn.State.ToString() == "Open")
-                conn.Close();
+            Methods.showMessageBox(Response, "对不起您没有权限");
+            return;
         }
+        string cname = couNameTB.Text.Trim();
+        string cterm = termTB.Text.Trim();
+        string ctype = typeTB.Text.Trim();
+        string cCreate = Session["ha_user"].ToString();
+        string selstr = "select max(couId) from Course";
+        string tid = tidTB.Text.Trim();
+        SqlParameter[] paras = new SqlParameter[1];
+        string ccouId = sqlhp.getAValue(selstr, paras);
+        if (ccouId == "")
+        { ccouId = "-1"; }
+        ccouId = (Convert.ToInt32(ccouId) + 1).ToString();
+        sqlhp.close();
+        if (Methods.mkCou(ccouId, cname, ctype, "1", cterm, cCreate, tid, Session["ha_user"].ToString(), Session["ha_pwd"].ToString()))
+        {
+
+            Methods.showMessageBox(Response, "添加成功");
+            Response.Redirect("\\admin/couseManage.aspx");
+        }
+        else
+        {
+            Methods.showMessageBox(Response, "添加失败");
+        }
+    }
+    protected void backBtn_Click(object sender, EventArgs e)
+    {
+        Panel3.Visible = false;
     }
 }
