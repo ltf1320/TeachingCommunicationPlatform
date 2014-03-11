871b54266a413a7182122f080f297e98094e72fd
ltf1320 <327105504@qq.com>
2014-03-11 21:35:03 +0800

    haha

--- TeachingCommunicationPlatform_web/Default.aspx.cs
+++ TeachingCommunicationPlatform_web/Default.aspx.cs
@@ -9,45 +9,7 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
-        DateTime DT = System.DateTime.Now;
-        string utime = System.DateTime.Now.ToString();
-        Response.Write(utime);
- //       safeFileManager sFM = new safeFileManager();
-        //if(sFM.SetRootPath("D:\\programdesign\\git\\TeachingCommunicationPlatform\\TeachingCommunicationPlatform_web\\severFiles\\users\\ltf\\aa"))
-        //    Response.Write("OK");
-        //else Response.Write("WTF");
-        //string haha="123456789aaa123456";
-        //Response.Write(haha.IndexOf("aaa"));
-
-        //sFM.setUser("00000", "11111");
-        //Response.Write(sFM.nUserType.ToString());
-
-        //SQLHelper sqlHelper=new SQLHelper();
-        //string sql = "update Course set term=20131 where couId=1";
-        //sqlHelper.ExecuteSql(sql, null);
-        /*
-        Session["ha_user"] = "00000";
-        Session["ha_pwd"] = "11111";
-        Session["ha_path"] = safeFileManager.getPath(safeFileManager.folderType.course, "1");
-        Response.Redirect("publicFunction\\visitPath.aspx");
-         * */
-        //safeFileManager sFM = new safeFileManager();
-        //sFM.setUser("00000", "11111");
-        //sFM.SetRootPath("courses");
-        //string[] txt=sFM.readFile("a.txt");
-        //for (int i = 0; i < txt.Length; i++)
-        //    Response.Write(txt[i]);
-  //      safeFileManager sFM = new safeFileManager();
-  //      sFM.setUser("00000", "11111");
-  //      sFM.SetRootPath("courses");
-  //      string[] txt=sFM.readFile("a.txt");
-  //      for (int i = 0; i < txt.Length; i++)
-  //          Response.Write(txt[i]);
-  ////      Response.Write(sFM.DeleteFile("a"));
-  //       * 
-  //      SQLHelper sqlHelper=new SQLHelper();
-  //      string sql = "select * from user";
-  //      sqlHelper.ExecuteSql(sql, null);
+        CMessage msg;
+        msg.createMsg("0", 0, "bool", new DateTime(2014, 3, 11, 21, 27, 10), null, "yiikou", null, null);
     }
 }
